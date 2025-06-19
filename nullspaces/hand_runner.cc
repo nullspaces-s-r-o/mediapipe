3f13af74683d08ccf0929e285a20027d76199435
Jiri Kula <jiri.kula@nullspaces.com>
2025-06-19 20:47:50 +0000

    build on ssd

--- nullspaces/hand_runner.cc
+++ nullspaces/hand_runner.cc
@@ -10,7 +10,7 @@ int main() {
   auto options = std::make_unique<hand_landmarker::HandLandmarkerOptions>();
   options->running_mode = core::RunningMode::IMAGE;
   options->base_options.model_asset_path =
-      "/home/radxa/ssd/nullspaces/mediapipe/hand_landmarker.task";  // Update with your model path
+      "/home/radxa/hand_landmarker.task";  // Update with your model path
 
   auto statusOrLandmarker =
       hand_landmarker::HandLandmarker::Create(std::move(options));
@@ -30,13 +30,22 @@ int main() {
   // Mat image(640, 480, CV_8UC3, Scalar(0, 0, 0));  // Create a black image
   // Mat image = imread("/home/jiri/google/hand.jpg");
   Mat image;
-  cv::VideoCapture cap(0);
+  cv::VideoCapture cap;
+  for (int i = 4; i < 10; ++i) {
+    cap.open(i);  // Try to open each camera index from 0 to 9
+    if (cap.isOpened()) {
+      std::cout << "Camera opened successfully at index: " << i << std::endl;
+      break;  // Exit loop if camera is opened successfully
+    } else {
+      std::cout << "Failed to open camera at index: " << i << std::endl;
+    }
+  }
   if (!cap.isOpened()) {
     std::cerr << "Error: Could not open camera." << std::endl;
     return -1;
   }
 
-  cv::namedWindow("Hand Landmarker", cv::WINDOW_AUTOSIZE);
+  // cv::namedWindow("Hand Landmarker", cv::WINDOW_AUTOSIZE);
 
   while (true) {
     cap >> image;  // Capture a single frame from the camera
@@ -60,10 +69,26 @@ int main() {
     }
     auto handLandmarkerResult = result.value();
 
-    imshow("Hand Landmarker", image);
-    if (waitKey(30) >= 0) {
-      break;  // Exit on any key press
+    if (handLandmarkerResult.hand_landmarks.empty()) {
+      std::cout << "No hand landmarks detected." << std::endl;
+    } else {
+      std::cout << "Detected " << handLandmarkerResult.hand_landmarks.size()
+                << " hands." << std::endl;
+
+      for (const auto& hand : handLandmarkerResult.hand_landmarks) {
+        for (const auto& landmark : hand.landmarks) {
+          cv::circle(
+              image,
+              cv::Point(landmark.x * image.cols, landmark.y * image.rows), 5,
+              cv::Scalar(0, 255, 0), -1);
+        }
+      }
     }
+
+    // imshow("Hand Landmarker", image);
+    // if (waitKey(30) >= 0) {
+    //   break;  // Exit on any key press
+    // }
   }
 
   return 0;
